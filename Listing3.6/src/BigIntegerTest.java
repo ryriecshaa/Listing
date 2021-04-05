8406ff00b884559f5de51fe354ad7c4c7b4d7484
ryriecshaa <76834995+ryriecshaa@users.noreply.github.com>
2021-04-05 17:21:06 +0700

    Add files via upload

---
+++ Listing3.6/src/BigIntegerTest.java
@@ -0,0 +1,20 @@
+import java.math.*;
+import java.util.*;
+
+public class BigIntegerTest {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("How many numbers do you need to draw? ");
+			int k = in.nextInt();
+		
+		System.out.print("What is the highest number you can draw? ");
+			int n = in.nextInt();
+		
+		BigInteger randoms = BigInteger.valueOf(1);
+		
+		for (int i = 1; i <= k; i++)
+			randoms = randoms.multiply(randoms.valueOf(n - i + 1)).divide(randoms.valueOf(i));
+		
+		System.out.println("Your odds are 1 in " + randoms + ". Good Luck!");
+	}
+}
